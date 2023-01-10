01e8fd7868965f647c8149be0d50eec26d4521d2
Anosha Rahim <anosharahim03@gmail.com>
2023-01-10 09:49:18 +0800

    create note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,24 @@
+import {MdDeleteForever} from 'react-icons/md';
+
 const Note () => {
     return(
         <div className='note'>
+        <span> Hello, this is our first note! </span> 
+        <div className="note-footer"> 
+            <small> date</small>
+            <MdDeleteForever className='delete-icon' size='1.3em'/>
+        </div>
+
         </div>
     );
 };
 
 export default Note;
+// you can make css files as long as some component calls on them 
+// you can only write css in css files, or style tags in  js files 
+// style=something directly influences style
+// course in javascript 
+// by the time i wake up, i want to be able to enter some data somewhere in the 
+// application where there is a data array, which should display the notes. 
+// If there are any concepts you don't understand what it is doing, you should look it up. 
+// Once I see the app, I will quiz you
