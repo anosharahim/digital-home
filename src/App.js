53936407ba54774482856e3adce819adba35df2e
Anosha Rahim <anosharahim03@gmail.com>
2023-01-10 10:06:34 +0800

    Add noteslist to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import NotesList from "./components/NotesList";
+import "./App.css";
+
 
 const App = () => {
-return(<p>hello react!</p>)
-}
+return <div className="container">
+    <NotesList />
+</div>;
+};
 
 export default App;
