77b3cef13d568a0e2d12a90cf4108de6a05cfd98
Anosha Rahim <anosharahim03@gmail.com>
2023-03-03 15:29:53 +0800

    add API endpoint URLs

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,4 +24,8 @@ router.register(r'notes', views.NotesView, 'note')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('login/', views.login_view, name='api-login'),
+    path('logout/', views.logout_view, name='api-logout'),
+    path('session/', views.session_view, name='api-session'),
+    path('whoami/', views.whoami_view, name='api-whoami')
 ]
